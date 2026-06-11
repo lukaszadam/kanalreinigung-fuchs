11d507852e282f7ab919d98026689ad2943eaf72
uxffm <35499917+uxffm@users.noreply.github.com>
2026-06-11 17:31:58 +0200

    dede

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -7,6 +7,8 @@ const staticPages = [
   { path: '/',                               priority: '1.0', changefreq: 'weekly'  },
   { path: '/kanalsanierung-frankfurt',                 priority: '0.9', changefreq: 'monthly' },
   { path: '/wasser-auslaufen-kueche-bad-frankfurt',   priority: '0.9', changefreq: 'monthly' },
+  { path: '/wc-verstopft-friedberg',                 priority: '0.9', changefreq: 'monthly' },
+  { path: '/wc-verstopft-bad-homburg',               priority: '0.9', changefreq: 'monthly' },
   { path: '/kanalreinigung-bad-nauheim',     priority: '0.9', changefreq: 'monthly' },
   { path: '/kanalreinigung-giessen',         priority: '0.9', changefreq: 'monthly' },
   { path: '/kanalreinigung-raunheim',        priority: '0.9', changefreq: 'monthly' },
